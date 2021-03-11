052409c889a6d7bd0bfda2792eac47baef45abcd
zibingala <47749140+zibingala@users.noreply.github.com>
2021-03-11 10:55:02 +0100

    js files

---
+++ js/FRONT.js
@@ -0,0 +1,16 @@
+function helloworldconsole1(string)
+{
+
+    console.log(string);
+
+    return null;
+}
+
+
+function helloworldconsole2(string)
+{
+    console.log("helloworldconsole2");
+    console.log(string);
+
+    return null;
+}
